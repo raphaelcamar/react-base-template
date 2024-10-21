4e02a838bba7ca35bd530a819a591e26dfa034ce
Raphael Camargo <raphaelcamar@outlook.com>
2024-10-21 19:47:02 -0300

    add tests support

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  verbose: true,
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!**/*.d.ts',
+    '!<rootDir>/src/**/protocols/**',
+    '!<rootDir>/src/**/icons/*-icon.tsx',
+    '!<rootDir>/src/**/styles.ts',
+    '!<rootDir>/src/**/pages/**/*',
+    '!<rootDir>/src/**/validation/**/*',
+    '!<rootDir>/src/**/validator/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!<rootDir>/src/**/imports.ts',
+    '!<rootDir>/src/**/types.ts',
+    '!<rootDir>/src/**/*.data.ts',
+    '!<rootDir>/src/monthly-recurrence/**/*',
+    // '!<rootDir>/src/transaction/**/*',
+    '!<rootDir>/src/user/**/*',
+    '!<rootDir>/src/**/*.d.*',
+    '!<rootDir>/src/**/*-error.*',
+  ],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/'
+  ],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    // '^.+\\.(css|less)$': '<rootDir>/config/CSSStub.js'
+  },
+  transform: {
+    '.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  // setupFilesAfterEnv: ["<rootDir>/.jest/setup-tests.jsx"],
+  // resolver: '<rootDir>/.jest/resolver.js',
+}
