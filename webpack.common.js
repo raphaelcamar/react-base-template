2313159f1b6be1c6be6989d0fd73dc7b5a76a13b
Raphael Camargo <raphaelcamar@outlook.com>
2024-10-21 15:18:01 -0300

    add webpack with basic configuration

---
+++ webpack.common.js
@@ -0,0 +1,80 @@
+
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const envFile = './.env'
+
+module.exports = {
+  entry: {
+    app: './src/main/index.tsx'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    clean: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@': path.join(__dirname, 'src'),
+      react: path.resolve('./node_modules/react')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)s(x?)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ],
+  },
+  devServer: {
+    static: './public',
+    historyApiFallback: true,
+    devMiddleware: {
+      writeToDisk: true
+    },
+    compress: true,
+    port: 3000,
+  },
+  optimization: {
+    runtimeChunk: true,
+    minimize: true,
+    emitOnErrors: true,
+    innerGraph: false,
+    mangleExports: 'size',
+    mangleWasmImports: true,
+    moduleIds: 'named'
+  },
+  performance: {
+    hints: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public/index.html'),
+      favicon: path.resolve(__dirname, 'public/img/favicon.ico'),
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+    new webpack.HotModuleReplacementPlugin({}),
+    new Dotenv({
+      path: envFile,
+      safe: true,
+      systemvars: true
+    }),
+    new BundleAnalyzerPlugin(),
+  ]
+}
